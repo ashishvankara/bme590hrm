31a4972b3571c5440678fa86114ef072edd0ae24
Ashish Vankara <ashishvankara@gmail.com>
2018-10-26 02:37:07 -0400

    added dataprocessing

--- readCSV.py
+++ readCSV.py
@@ -26,9 +26,7 @@ def readCsv(name):
 
         raise TypeError("Length of time column is unequal"
                         "to the length of the voltage column")
-    print(t)
-    print(v)
-    return [t, v]
+        return [t, v]
 
 
 if __name__ == "__main__":
