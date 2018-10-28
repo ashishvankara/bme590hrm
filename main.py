2ac14d05190e37de2772ee7a613398ee5216f040
Ashish Vankara <ashishvankara@gmail.com>
2018-10-28 13:31:10 -0400

    Further modifications to include hrcalc to main

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ logging.basicConfig(filename='log.txt', level=logging.DEBUG)
 
 def main():
     from readCSV import readCsv
+    csvfile = r'test_data\test_data1.csv'
+
+    [t, v] = readCsv(csvfile)
+    bottomtimebnd = 0
+    toptimebnd =
+
     import json
     from processECG import subtractDC
     from processECG import savgolFilter
     from processECG import movingAverage
     from extVolt import extremeVolt
     from timeDuration import timeDuration
-    [t,v] = readCsv(r'test_data\test_data1.csv')
+
     sv = subtractDC(v)
     svf = savgolFilter(sv)
     avgvolt = movingAverage(t, svf, 30)
