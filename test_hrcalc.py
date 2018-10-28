8aa60cb5e8377bcc2b0662dc3910e2c514200672
Ashish Vankara <ashishvankara@gmail.com>
2018-10-28 13:04:10 -0400

    Added average hr calculation function

---
+++ test_hrcalc.py
@@ -0,0 +1,14 @@
+import pytest
+
+def test_hrcalc():
+    """" Tests average hr calculation
+
+    """
+    import numpy as np
+    from hrcalc import hrCalc
+    peakind = np.arange(1, 61)
+    t = np.arange(1, 61)
+    assert pytest.approx(hrCalc(peakind, t)) == 60
+
+if __name__ == "__main__":
+    test_hrcalc()
